40e69f60defbfc11e2f9b5e5c8ef42ff0e72ff6f
Anshul Sirur <vixus0@gmail.com>
2019-06-06 15:27:28 +0100

    Simplify layers and stop using mod-tap

--- keyboards/planck/keymaps/vixus0/keymap.c
+++ keyboards/planck/keymaps/vixus0/keymap.c
@@ -21,10 +21,8 @@ extern keymap_config_t keymap_config;
 
 enum planck_layers {
   _QWERTY,
-  _LOWER,  // symbols
-  _RAISE,  // numbers and Fkeys
-  _MOUSE,  // mouse controls
-  _MOVE,   // movement and WM
+  _LOWER,
+  _RAISE,
   _PLOVER,
   _ADJUST
 };
@@ -40,75 +38,45 @@ enum planck_keycodes {
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
-#define MOUSE MO(_MOUSE)
-#define MOVE  MO(_MOVE)
-
-#define NAV MO(_NAV)
-#define KCNB KC_NUBS
-#define KCNH KC_NUHS
-
-#define __L_1__ LALT_T(KC_TAB)
-#define __L_2__ LCTL_T(KC_ESC)
-#define __L_3__ KC_LSFT
-#define __L_4__ KC_LSFT
-
-#define __R_1__ LALT_T(KC_BSPC)
-#define __R_2__ LCTL_T(KC_QUOT)
-#define __R_3__ RSFT_T(KC_ENT)
 
 #define __MOD3__ KC_LCTL, KC_LGUI, KC_LALT
 #define __MOD4__ KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT
-#define __NAVM__ LALT_T(KC_LEFT), LGUI_T(KC_DOWN), LCTL_T(KC_UP), LSFT_T(KC_RIGHT)
+
 #define __NAV1__ KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
 #define __NAV2__ KC_HOME, KC_PGDN, KC_PGUP, KC_END
 #define __NAV3__ KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 
+#define KCNB KC_NUBS
+#define KCNH KC_NUHS
 #define MY_PIPE S(KCNB)
 #define MY_STLD S(KCNH)
 #define MY_AT   KC_DQT
 #define MY_DQT  S(KC_2)
+#define MY_QUID KC_HASH
+#define MY_ESC LGUI_T(KC_ESC)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty */
 [_QWERTY] = GRID(
-    __L_1__, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    __R_1__,
-    __L_2__, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, __R_2__,
-    __L_3__, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, __R_3__,
-    __L_4__,        __MOD3__          , LOWER,   KC_SPC,  KC_SPC,  RAISE,                __NAV1__
+    KC_TAB , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    MY_ESC , KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    KC_LSFT,        __MOD3__          , LOWER,   KC_LCTL, KC_SPC,  RAISE,                __NAV1__
 ),
 
-/* LOWER */
 [_LOWER] = GRID(
-    KC_GRV , KC_EXLM, MY_DQT , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS, KC_DEL ,
-    _______, KCNB   , KC_SLSH, MY_STLD, KC_UNDS, KC_MINS, KC_EQL , KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN, KC_UNDS,
-    _______, KCNB   , MY_AT  , KCNH   , MY_PIPE, KC_PLUS, KC_QUOT, KC_LCBR, KC_LT  , KC_GT  , KC_RCBR, KC_TILD,
-    _______, _______, _______, _______, _______, MOVE   , MOVE   , _______,              __NAV2__
-),
-
-/* RAISE */
-[_RAISE] = GRID(
-    KC_MUTE, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_EQL ,
-    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_4   , KC_5   , KC_6   , KC_MINS, KC_PLUS,
-    _______, KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11,  KC_F12 , KC_1   , KC_2   , KC_3   , KC_SLSH, KC_ASTR,
-    _______, _______, _______, _______, _______, MOUSE  , MOUSE  , _______,              __NAV3__
-),
-
-/* MOVE: Movement and window manager keys */
-[_MOVE] = GRID(
-    _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______, _______,
-    _______,              __MOD4__             , _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_TAB , _______,
-    _______, _______, _______, _______, _______, _______, KC_INS , KC_DEL , _______, _______, KC_BSPC, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-),
-
-/* MOUSE: Mouse control */
-[_MOUSE] = GRID(
     KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
-    _______, KC_ACL0, KC_ACL1, KC_ACL2, KC_BTN1, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN4, KC_BTN5,
-    _______, _______, _______, _______, _______, KC_BTN3, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______,              __MOD4__             , _______,             __NAV1__              , _______, KC_DEL ,
+    _______, _______, _______, _______, _______, _______,             __NAV2__              , _______, KC_ENT ,
+    _______, _______, _______, _______, _______, KC_INS , KC_DEL , _______,              __NAV2__
+),
+
+[_RAISE] = GRID(
+    KC_GRV , KC_EXLM, MY_DQT , KC_NUHS, KC_DLR , KC_PERC, KC_CIRC, KC_7   , KC_8   , KC_9   , KC_0   , KC_MUTE,
+    _______, KC_LPRN, KC_RPRN, KC_NUBS, MY_PIPE, KC_AMPR, KC_ASTR, KC_4   , KC_5   , KC_6   , KC_MINS, KC_PLUS,
+    _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, MY_STLD, MY_QUID, KC_1   , KC_2   , KC_3   , KC_UNDS, KC_EQL ,
+    _______, _______, _______, _______, _______, _______, _______, _______,              __NAV3__
 ),
 
 /* Plover layer (http://opensteno.org) */
